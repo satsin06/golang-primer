5903ef2fd135716d6822a79b0d77fe6d056ead45
Satyam Sinha <satyamsinha9404@gmail.com>
2023-09-11 14:59:40 +0530

    Multiple people greetings

--- hello/hello.go
+++ hello/hello.go
@@ -18,21 +18,34 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Println(messages)
+
 	// Request a greeting message.
 	
 	// message := greetings.Hello("Gladys")
 
 	// message, err := greetings.Hello("")
 
-	message, err := greetings.Hello("Gladys")
+	// message, err := greetings.Hello("Gladys")
 	
 	// If an error was returned, print it to the console and
 	// exit the program.
-	if err != nil {
-		log.Fatal(err)
-	}
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
 	// If no error was returned, print the returned message
 	// to the console.
-	fmt.Println(message)
+	// fmt.Println(message)
 }
