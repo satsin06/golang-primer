f32e03dd444fe4bc97ad1585209814055cb22de3
Satyam Sinha <satyamsinha9404@gmail.com>
2023-09-11 16:54:18 +0530

    Add test for greetings

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -20,6 +20,7 @@ func Hello(name string) (string, error) {
 	
 	// Create a message using a random format.
 	message := fmt.Sprintf(randomFormat(), name)
+	// message := fmt.Sprint(randomFormat())
 	return message, nil
 }
 
