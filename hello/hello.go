5778988b0eb54573f403f51a901a3b6bac96b696
Satyam Sinha <satyamsinha9404@gmail.com>
2023-09-11 12:01:17 +0530

    Call greetings module in Hello file

--- hello/hello.go
+++ hello/hello.go
@@ -1,10 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-import "rsc.io/quote"
+	"example.com/greetings"
+)
+
+// import "rsc.io/quote"
 
 func main() {
 	// fmt.Println("Hello, World!")
-	fmt.Println(quote.Go())
+	message := greetings.Hello("Gladys")
+	fmt.Println(message)
 }
